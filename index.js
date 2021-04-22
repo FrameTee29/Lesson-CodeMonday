53d52a8c9b9540e0ea09ca58406764491442b2ab
frametee29 <teeraphat123@hotmail.com>
2021-04-22 21:25:22 +0700

    path api

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 app.use('/api/teacher', TeacherController)
-app.use('/api/teacher', CourseController)
+app.use('/api/course', CourseController)
 
 
 app.get("/", (_, res) =>
